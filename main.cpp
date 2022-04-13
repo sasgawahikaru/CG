de8809383167d84d666242e2c931176e02f5bf87
sasagawahikaru <k021g1383@g.neec.ac.jp>
2022-04-13 14:51:27 +0900

    01_01_Windowsアプリケーション

--- main.cpp
+++ main.cpp
@@ -1,8 +1,61 @@
 #include<Windows.h>
 
+LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	//メッセージに応じた固有処理
+	switch (msg) {
+		//ウインドウ破壊
+	case WM_DESTROY:
+		//アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	OutputDebugStringA("114514 Hello,DirectX 415411!!\n");
+	const int window_width = 1280;
+	const int window_height = 720;
 
+	WNDCLASSEX w{};
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProc;
+	w.lpszClassName = L"DirectXgame";
+	w.hInstance = GetModuleHandle(nullptr);
+	w.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+	RegisterClassEx(&w);
+
+	RECT wrc = { 0,0,window_width,window_height };
+
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	HWND hwnd = CreateWindow(w.lpszClassName,
+		L"DirectXgame",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrc.right - wrc.left,
+		wrc.bottom - wrc.top,
+		nullptr,
+		nullptr,
+		w.hInstance,
+		nullptr);
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg{};
+	//
+	//
+
+	//ゲームループ
+	while (true) {
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		if (msg.message == WM_QUIT) {
+			break;
+		}
+	}
 	return 0;
 }
